66b05142f9f679ec6a981395f784294b6a8aa649
なまけもの <68767710+namakemono-san@users.noreply.github.com>
2024-09-03 16:00:48 +0900

    Feat: remixで作り直した

--- README.md
+++ README.md
@@ -1,40 +1,8 @@
-# Welcome to Remix!
+# <div align="center">nmkmn.moe</div>
 
-- 📖 [Remix docs](https://remix.run/docs)
+このリポジトリは、自分のポートフォリオを掲載しています。<br>
+Remix の練習も含めて作成いたしました。
 
-## Development
+## License
 
-Run the dev server:
-
-```shellscript
-npm run dev
-```
-
-## Deployment
-
-First, build your app for production:
-
-```sh
-npm run build
-```
-
-Then run the app in production mode:
-
-```sh
-npm start
-```
-
-Now you'll need to pick a host to deploy it to.
-
-### DIY
-
-If you're familiar with deploying Node applications, the built-in Remix app server is production-ready.
-
-Make sure to deploy the output of `npm run build`
-
-- `build/server`
-- `build/client`
-
-## Styling
-
-This template comes with [Tailwind CSS](https://tailwindcss.com/) already configured for a simple default starting experience. You can use whatever css framework you prefer. See the [Vite docs on css](https://vitejs.dev/guide/features.html#css) for more information.
+This project is licensed under the MIT License, see the [LICENSE](LICENSE) file for details
--- app/root.tsx
+++ app/root.tsx
@@ -1,15 +1,35 @@
+import { json } from "@remix-run/node";
 import {
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
+  useLoaderData,
+  useLocation,
 } from "@remix-run/react";
-import "./tailwind.css";
+import { useEffect } from "react";
+
+import "./css/tailwind.css";
+
+import * as gtag from "./core/utils/gtags.client";
+
+export async function loader() {
+  return json({ gaTrackingId: process.env.GA_TRACKING_ID });
+}
 
 export function Layout({ children }: { children: React.ReactNode }) {
+  const { gaTrackingId } = useLoaderData<typeof loader>();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (gaTrackingId?.length) {
+      gtag.pageview(location.pathname, gaTrackingId);
+    }
+  }, [location, gaTrackingId]);
+
   return (
-    <html lang="en">
+    <html lang="ja">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
@@ -18,6 +38,28 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         {children}
+        {!gaTrackingId ? null : (
+          <>
+            <script
+              async
+              src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`}
+            />
+            <script
+              async
+              id="gtag-init"
+              dangerouslySetInnerHTML={{
+                __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${gaTrackingId}', {
+                  page_path: window.location.pathname,
+                });
+              `,
+              }}
+            />
+          </>
+        )}
         <ScrollRestoration />
         <Scripts />
       </body>
