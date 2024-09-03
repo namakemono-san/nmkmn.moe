11167f264427de0aa56442cac250353d074dd578
なまけもの <68767710+namakemono-san@users.noreply.github.com>
2024-09-04 01:20:46 +0900

    Feat: discord用

--- app/root.tsx
+++ app/root.tsx
@@ -1,33 +1,14 @@
-import { json } from "@remix-run/node";
 import {
   Links,
   Meta,
   Outlet,
   Scripts,
   ScrollRestoration,
-  useLoaderData,
-  useLocation,
 } from "@remix-run/react";
-import { useEffect } from "react";
 
 import "./css/tailwind.css";
 
-import * as gtag from "./core/utils/gtags.client";
-
-export async function loader() {
-  return json({ gaTrackingId: process.env.GA_TRACKING_ID });
-}
-
 export function Layout({ children }: { children: React.ReactNode }) {
-  const { gaTrackingId } = useLoaderData<typeof loader>();
-  const location = useLocation();
-
-  useEffect(() => {
-    if (gaTrackingId?.length) {
-      gtag.pageview(location.pathname, gaTrackingId);
-    }
-  }, [location, gaTrackingId]);
-
   return (
     <html lang="ja">
       <head>
@@ -38,28 +19,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
       </head>
       <body>
         {children}
-        {!gaTrackingId ? null : (
-          <>
-            <script
-              async
-              src={`https://www.googletagmanager.com/gtag/js?id=${gaTrackingId}`}
-            />
-            <script
-              async
-              id="gtag-init"
-              dangerouslySetInnerHTML={{
-                __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${gaTrackingId}', {
-                  page_path: window.location.pathname,
-                });
-              `,
-              }}
-            />
-          </>
-        )}
         <ScrollRestoration />
         <Scripts />
       </body>
