7d5295f822ee56273e5506209d6d85433813f2f8
なまけもの <68767710+namakemono-san@users.noreply.github.com>
2024-09-04 14:11:32 +0900

    Fix: 認証されたのでいらない

--- app/routes/$.tsx
+++
@@ -1,8 +0,0 @@
-export const loader = () => {
-  const content = "dh=7fccce7e28c369c05310b1703d701323a27a08ea";
-
-  return new Response(content, {
-    status: 200,
-    headers: { "Content-Type": "plain/text" },
-  });
-};
