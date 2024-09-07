236c597b34e541fb053b9313c9cc7aca7af9920f
なまけもの <68767710+namakemono-san@users.noreply.github.com>
2024-09-08 00:33:14 +0900

    Fix: meta

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,4 @@
-// import type { MetaFunction } from "@remix-run/node";
+import type { MetaFunction } from "@remix-run/node";
 import {
   Links,
   Meta,
@@ -9,34 +9,34 @@ import {
 
 import "./css/tailwind.css";
 
-// export const meta: MetaFunction = () => {
-//   return [
-//     { property: "twitter:image", content: "https://nmkmn.moe/images/bg_1.png" },
-//     { property: "twitter:card", content: "summary_large_image" },
-//     { property: "twitter:title", content: "namakemono" },
-//     { property: "twitter:description", content: "Hi! I'm namakemono :)" },
-//     { title: "nmkmn.moe" },
-//     {
-//       name: "description",
-//       content: "Hi! I'm namakemono :)",
-//     },
-//     {
-//       name: "theme-color",
-//       content: "#b4f0faa8",
-//     },
-//     { property: "og:image", content: "https://nmkmn.moe/images/bg_1.png" },
-//     { property: "og:site_name", content: "nmkmn.moe" },
-//     { property: "og:title", content: "namakemono" },
-//     {
-//       property: "og:description",
-//       content: "Hi! I'm namakemono :)",
-//     },
-//     {
-//       property: "og:url",
-//       content: "https://nmkmn.moe",
-//     },
-//   ];
-// };
+export const meta: MetaFunction = () => {
+  return [
+    { property: "twitter:image", content: "https://nmkmn.moe/images/bg_1.png" },
+    { property: "twitter:card", content: "summary_large_image" },
+    { property: "twitter:title", content: "namakemono" },
+    { property: "twitter:description", content: "Hi! I'm namakemono :)" },
+    { title: "nmkmn.moe" },
+    {
+      name: "description",
+      content: "Hi! I'm namakemono :)",
+    },
+    {
+      name: "theme-color",
+      content: "#b4f0faa8",
+    },
+    { property: "og:image", content: "https://nmkmn.moe/images/bg_1.png" },
+    { property: "og:site_name", content: "nmkmn.moe" },
+    { property: "og:title", content: "namakemono" },
+    {
+      property: "og:description",
+      content: "Hi! I'm namakemono :)",
+    },
+    {
+      property: "og:url",
+      content: "https://nmkmn.moe",
+    },
+  ];
+};
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -44,23 +44,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-
-        <meta
-          property="twitter:image"
-          content="https://nmkmn.moe/images/bg_1.png"
-        />
-        <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:title" content="namakemono" />
-        <meta property="twitter:description" content="Hi! I'm namakemono :)" />
-        <title>nmkmn.moe</title>
-        <meta name="description" content="Hi! I'm namakemono :)" />
-        <meta name="theme-color" content="#b4f0faa8" />
-        <meta property="og:image" content="https://nmkmn.moe/images/bg_1.png" />
-        <meta property="og:site_name" content="nmkmn.moe" />
-        <meta property="og:title" content="namakemono" />
-        <meta property="og:description" content="Hi! I'm namakemono :)" />
-        <meta property="og:url" content="https://nmkmn.moe" />
-
         <Meta />
         <Links />
       </head>
