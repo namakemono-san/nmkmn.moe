57953abbf829e1d570eb9c89a222dd16d3de7b2d
なまけもの <68767710+namakemono-san@users.noreply.github.com>
2025-03-15 11:55:32 +0900

    chore: upgrade

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,5 @@
 import { vitePlugin as remix } from "@remix-run/dev";
-
-//@ts-expect-error 形宣言エラーが出るため
 import { defineConfig } from "vite";
-//@ts-expect-error 形宣言エラーが出るため
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
